59db48dbdaa088f2397af6e8a94c91534970e460
Dennis Murray <murraden@gmail.com>
2023-11-29 11:27:35 -0800

    have a look

--- fetch.js
+++ fetch.js
@@ -1,9 +1,29 @@
 // create api-key.js file with const API_KEY="your_api_key" in this same directory to use
-const BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+//const BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
 
-const url = `${BASE_URL}?q=tech&api-key=${API_KEY}`;
+const API_KEY = 'LlsFqCHJDASo4ecWdF0MgASzPdqbEzZ1'
+const date = '2022-03-12'
+const list = 'hardcover-fiction'
+const booksURL = `https://api.nytimes.com/svc/books/v3/lists/${date}/${list}.json?api-key=${API_KEY}`
 
-fetch(url)
+//const url = `${booksURL}?q=tech&api-key=${API_KEY}`;
+
+/*const asyncFetch = async function () {
+  try {
+    const data = await fetch(booksURL)
+
+    const dataJson = await data.json()
+
+    console.log("Async / Await func", data)
+
+    console.log("Async / Await func", dataJson)
+  } catch (err) {
+    console.log(err)
+  }
+} */
+
+
+fetch(booksURL)
   .then(function(data) {
     return data.json();
   })
@@ -20,4 +40,4 @@ fetch(url)
       const imgUrl = `https://www.nytimes.com/${article.multimedia[0].url}`;
       document.getElementById('article-img').src = imgUrl;
     }
-  });
+  }); 
